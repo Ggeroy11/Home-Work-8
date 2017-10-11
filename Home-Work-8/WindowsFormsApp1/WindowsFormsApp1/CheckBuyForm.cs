73ac9736954695fb84d7255e00390545d1a48eac
Ggeroy11 <elnar1995@mail.ru>
2017-10-11 13:44:33 +0600

    Home Work 8
    реализовано сохранение в  list коллекцию

---
+++ Home-Work-8/WindowsFormsApp1/WindowsFormsApp1/CheckBuyForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    //Фирма продает составляющие компьютера. Первая форма отвеча-
+    // ет за учет продаж, вторая за добавление и редактирование составля-
+    //ющих.
+    public partial class CheckBuyForm : Form
+    {
+        public string product;
+        AddEditForm addEdit;
+        List<string> baket =new List<string>() { "Monitor","Mouse","Proccesor","Keyboard"};
+        public CheckBuyForm()
+        {
+            InitializeComponent();
+            foreach (string a in baket)
+            {
+                comboBox1.Items.Add(a);
+            }
+            addEdit = new AddEditForm(this);
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            product=textBox1.Text;
+            addEdit.Addproduct(product);
+            addEdit.Show();
+            baket.Add(product);
+        }
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            if (baket.=="Mouse") { 
+                
+                }
+        }
+    }
+}
